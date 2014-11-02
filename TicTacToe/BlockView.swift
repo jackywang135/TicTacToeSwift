c734676a61b03d16bb7a8baae1f4145b4633123d
jackywang135 <jackywang135@gmail.com>
2014-11-02 02:12:29 -0800

    Finished animating O and X, defining enum Turn

---
+++ TicTacToe/BlockView.swift
@@ -0,0 +1,140 @@
+//
+//  BlockView.swift
+//  TicTacToe
+//
+//  Created by Jacky Wang on 10/31/14.
+//  Copyright (c) 2014 JACKYWANG. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreGraphics
+
+
+enum Mark {
+    case X, O, Empty
+}
+
+class BlockView : UIView {
+    
+    init(mark : Mark) {
+        super.init()
+//        self.mark = mark
+//        draw(mark)
+    }
+    
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+    }
+    
+    var mark : Mark = .Empty
+    let animationDuration = 0.5
+    let lineWidth : CGFloat = 7
+    
+    func draw (mark : Mark) {
+        if mark == .X {
+            self.mark = .X
+            drawX()
+        }
+        if mark == .O {
+            self.mark = .O
+            drawO()
+        }
+    }
+    
+    func drawX() {
+        
+        var crossShapeLayer = CAShapeLayer()
+        let center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds))
+        let r : CGFloat = (self.bounds.size.height <= self.bounds.size.width) ? self.bounds.size.height * CGFloat(0.75/2) : self.bounds.size.width * CGFloat(0.75/2)
+        
+        let topLeftPoint = CGPointMake(center.x - r * sqrt(2)/2, center.y - r * sqrt(2)/2)
+        let topRightPoint = CGPointMake(center.x + r * sqrt(2)/2, center.y - r * sqrt(2)/2)
+        let botLeftPoint = CGPointMake(center.x - r * sqrt(2)/2, center.y + r * sqrt(2)/2)
+        let botRightPoint = CGPointMake(center.x + r * sqrt(2)/2, center.y + r * sqrt(2)/2)
+        
+        var crossPath = UIBezierPath()
+        crossPath.moveToPoint(topLeftPoint)
+        crossPath.addLineToPoint(botRightPoint)
+        crossPath.moveToPoint(topRightPoint)
+        crossPath.addLineToPoint(botLeftPoint)
+        
+        crossShapeLayer.path = crossPath.CGPath
+        crossShapeLayer.lineWidth = lineWidth
+        crossShapeLayer.strokeColor = UIColor.blackColor().CGColor
+        
+        let pencilImage = UIImage(named: "Pencil")
+        var pencilLayer = CALayer()
+        pencilLayer.contents = pencilImage!.CGImage
+        pencilLayer.anchorPoint = CGPointMake(0, 0)
+        pencilLayer.frame = CGRectMake(-600, -600, pencilImage!.size.width, pencilImage!.size.height)
+        
+        var crossAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        crossAnimation.duration = animationDuration
+        crossAnimation.fromValue = 0
+        crossAnimation.toValue = 1
+        
+        var penAnimation = CAKeyframeAnimation(keyPath: "position")
+        penAnimation.duration = crossAnimation.duration
+        penAnimation.path = crossPath.CGPath
+        penAnimation.fillMode = kCAFillModeBoth
+        
+        crossShapeLayer.addSublayer(pencilLayer)
+        self.layer.addSublayer(crossShapeLayer)
+        crossShapeLayer.addAnimation(crossAnimation, forKey: "drawCrossAnimation")
+        pencilLayer.addAnimation(penAnimation, forKey: "drawPenAnimation")
+        
+        
+    }
+    
+    
+    
+    func drawO() {
+        
+        var circleShapeLayer = CAShapeLayer()
+        
+        let r : CGFloat = (self.bounds.size.height <= self.bounds.size.width) ? self.bounds.size.height * CGFloat(0.75/2) : self.bounds.size.width * CGFloat(0.75/2)
+        
+        let center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds))
+        
+        var circlePath = UIBezierPath(arcCenter: center, radius: r, startAngle: 0, endAngle: CGFloat(2*(M_PI)), clockwise: true)
+        circleShapeLayer.path = circlePath.CGPath
+        circleShapeLayer.strokeColor = UIColor.blackColor().CGColor
+        circleShapeLayer.fillColor = nil
+        circleShapeLayer.lineWidth = lineWidth
+        
+        let pencilImage = UIImage(named: "Pencil")
+        var pencilLayer = CALayer()
+        pencilLayer.contents = pencilImage!.CGImage
+        pencilLayer.anchorPoint = CGPointMake(0, 0)
+        pencilLayer.frame = CGRectMake(-600, -600, pencilImage!.size.width, pencilImage!.size.height)
+        
+        var circleAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        circleAnimation.fromValue = 0
+        circleAnimation.toValue = 1
+        circleAnimation.duration = animationDuration
+        
+        var penAnimation = CAKeyframeAnimation(keyPath: "position")
+        penAnimation.path = circlePath.CGPath
+        penAnimation.duration = circleAnimation.duration
+        penAnimation.fillMode = kCAFillModeBoth
+        
+        self.layer.addSublayer(circleShapeLayer)
+        circleShapeLayer.addSublayer(pencilLayer)
+        
+        circleShapeLayer.addAnimation(circleAnimation, forKey: "drawCircleAnimation")
+        pencilLayer.addAnimation(penAnimation, forKey: "drawPenAnimation")
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,39 +9,107 @@
 import UIKit
 import QuartzCore
 
+enum Turn {
+    case Xturn, Oturn
+}
+
+enum GameStatus {
+    case Xwin, Owin, tie, inconclusive
+}
+
+extension Turn {
+    
+    func switchTurn() -> Turn {
+        switch self {
+        case Xturn:
+            return .Oturn
+        case Oturn:
+            return .Xturn
+        }
+    }
+    
+    func getMark() -> Mark {
+        switch self {
+        case Xturn:
+            return .X
+        case Oturn:
+            return .O
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     required init(coder aDecoder: NSCoder) {
+        turn = .Xturn
+        gameStatus = .inconclusive
+        
         super.init(coder: aDecoder)
         
     }
+    
+    var turn : Turn
+    
+    var gameStatus : GameStatus
+    
     var arrayOfLines : [Line] {
         get {
-            let screenHeight = CGFloat(UIScreen.mainScreen().bounds.height)
-            let screenWidth = CGFloat(UIScreen.mainScreen().bounds.width)
-            
-            let lineVerticalLeft = Line(point1: CGPointMake(screenWidth/3, 0), point2: CGPointMake(screenWidth/3, screenHeight))
-            let lineVerticalRight = Line(point1: CGPointMake(screenWidth*2/3, 0), point2: CGPointMake(screenWidth*2/3, screenHeight))
-            let lineHorizonatalTop = Line(point1: CGPointMake(0, screenHeight/3), point2: CGPointMake(screenWidth, screenHeight/3))
-            let lineHorizontalBottom = Line(point1: CGPointMake(0, screenHeight*2/3), point2: CGPointMake(screenWidth, screenHeight*2/3))
-            
-            return [lineHorizonatalTop, lineHorizontalBottom, lineVerticalLeft, lineVerticalRight]
+            return setUpLines()
         }
     }
- 
+    
+    let winningBlockCombination = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    
+    func setUpLines() -> [Line] {
+        let screenHeight = CGFloat(UIScreen.mainScreen().bounds.height)
+        let screenWidth = CGFloat(UIScreen.mainScreen().bounds.width)
+        
+        let lineVerticalLeft = Line(point1: CGPointMake(screenWidth/3, 0), point2: CGPointMake(screenWidth/3, screenHeight))
+        let lineVerticalRight = Line(point1: CGPointMake(screenWidth*2/3, 0), point2: CGPointMake(screenWidth*2/3, screenHeight))
+        let lineHorizonatalTop = Line(point1: CGPointMake(0, screenHeight/3), point2: CGPointMake(screenWidth, screenHeight/3))
+        let lineHorizontalBottom = Line(point1: CGPointMake(0, screenHeight*2/3), point2: CGPointMake(screenWidth, screenHeight*2/3))
+        
+        return [lineHorizonatalTop, lineHorizontalBottom, lineVerticalLeft, lineVerticalRight]
+    }
+    
+    @IBOutlet var blockViewCollection: Array <BlockView>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        drawLines(0)
+        drawLinesAnimation(0)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func drawLines(lineCount:Int) {
+    @IBAction func tap(sender: UITapGestureRecognizer) {
+        
+        let point = sender.locationInView(self.view)
+        var blockView = self.view.hitTest(point, withEvent:nil)!
+        
+        if let blockView = blockView as? BlockView {
+            if blockView.mark == .Empty {
+                blockView.draw(turn.getMark())
+                turn = turn.switchTurn()
+            }
+        }
+    }
+    
+    func createWinningCombo() {
+        for blockview in blockViewCollection {
+            
+        }
+    }
+    
+    func checkWin() {
+        for blockview in blockViewCollection {
+
+        }
+    }
+    
+    func drawLinesAnimation(lineCount:Int) {
         
         var count = lineCount
         let line = arrayOfLines[count]
@@ -56,8 +124,8 @@ class ViewController: UIViewController {
             var pencilLayer = CALayer()
             pencilLayer.contents = pencilImage!.CGImage
             pencilLayer.anchorPoint = CGPointMake(0, 0)
-            pencilLayer.frame = CGRectMake(0, 0, pencilImage!.size.width, pencilImage!.size.height)
-            
+            pencilLayer.frame = CGRectMake(-600, -600, pencilImage!.size.width, pencilImage!.size.height)
+        
             lineShapeLayer.addSublayer(pencilLayer)
             
             var lineAnimation = CABasicAnimation(keyPath: "strokeEnd")
@@ -67,7 +135,7 @@ class ViewController: UIViewController {
             
             var penAnimation = CAKeyframeAnimation(keyPath: "position")
             penAnimation.duration = lineAnimation.duration
-            penAnimation.fillMode = kCAFillModeForwards
+            penAnimation.fillMode = kCAFillModeBoth
             penAnimation.calculationMode = kCAAnimationCubicPaced
             penAnimation.path = lineShapeLayer.path
 
@@ -78,7 +146,7 @@ class ViewController: UIViewController {
                 pencilLayer.removeFromSuperlayer()
                 count = count + 1
                 if count < self.arrayOfLines.count {
-                    self.drawLines(count)
+                    self.drawLinesAnimation(count)
                 }
             }
             
@@ -87,5 +155,7 @@ class ViewController: UIViewController {
             
             CATransaction.commit()
     }
+    
+    
 }
 
